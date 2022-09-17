84b14ab65085e7c9797f41c8eb5ef14295c38753
Vsev0l0d <vsevolod628@gmail.com>
2022-09-17 13:33:34 +0300

    s

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import string
 
-FIRST_SYMBOL = 'А'
-LAST_SYMBOL = 'Я'
-ALPHABET_LENGTH = ord(LAST_SYMBOL) - ord(FIRST_SYMBOL) + 1
+ALPHABET_LENGTH = 0x10FFFF + 1
 
 
 def shift(symbol: string, number: int) -> string:
-    if not (FIRST_SYMBOL <= symbol <= LAST_SYMBOL or FIRST_SYMBOL.lower() <= symbol <= LAST_SYMBOL.lower()):
-        char = chr(ord(symbol) + number)
-        while FIRST_SYMBOL <= char <= LAST_SYMBOL or FIRST_SYMBOL.lower() <= char <= LAST_SYMBOL.lower():
-            char = chr(ord(char) + ALPHABET_LENGTH)
-        return char
-
-    if ord(symbol.upper()) + number < ord(FIRST_SYMBOL):
-        return chr(ord(symbol) + number + ALPHABET_LENGTH)
-    elif ord(symbol.upper()) + number > ord(LAST_SYMBOL):
+    if ord(symbol) + number >= ALPHABET_LENGTH:
         return chr(ord(symbol) + number - ALPHABET_LENGTH)
-
+    elif ord(symbol) + number < 0:
+        return chr(ord(symbol) + number + ALPHABET_LENGTH)
     return chr(ord(symbol) + number)
 
 
 def encrypt(text: string, key: string, reverse: bool = False) -> string:
     ciphertext = ''
     for i, symbol in enumerate(text):
-        number = ord(key[i % len(key)].upper()) - ord(FIRST_SYMBOL)
+        number = ord(key[i % len(key)])
         if reverse:
             number = -number
         ciphertext += shift(symbol, number)
