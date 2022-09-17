f07c1d4e19ec12def5fcb15bf5f38973a9b576f3
Vsev0l0d <vsevolod628@gmail.com>
2022-09-17 13:22:31 +0300

    s

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import string
+
 FIRST_SYMBOL = 'А'
 LAST_SYMBOL = 'Я'
 ALPHABET_LENGTH = ord(LAST_SYMBOL) - ord(FIRST_SYMBOL) + 1
 
 
-def shift(symbol, number):
+def shift(symbol: string, number: int) -> string:
+    if not (FIRST_SYMBOL <= symbol <= LAST_SYMBOL or FIRST_SYMBOL.lower() <= symbol <= LAST_SYMBOL.lower()):
+        char = chr(ord(symbol) + number)
+        while FIRST_SYMBOL <= char <= LAST_SYMBOL or FIRST_SYMBOL.lower() <= char <= LAST_SYMBOL.lower():
+            char = chr(ord(char) + ALPHABET_LENGTH)
+        return char
+
     if ord(symbol.upper()) + number < ord(FIRST_SYMBOL):
         return chr(ord(symbol) + number + ALPHABET_LENGTH)
     elif ord(symbol.upper()) + number > ord(LAST_SYMBOL):
@@ -12,7 +20,7 @@ def shift(symbol, number):
     return chr(ord(symbol) + number)
 
 
-def encrypt(text, key, reverse=False):
+def encrypt(text: string, key: string, reverse: bool = False) -> string:
     ciphertext = ''
     for i, symbol in enumerate(text):
         number = ord(key[i % len(key)].upper()) - ord(FIRST_SYMBOL)
@@ -22,10 +30,19 @@ def encrypt(text, key, reverse=False):
     return ciphertext
 
 
-def decrypt(ciphertext, key):
+def decrypt(ciphertext: string, key: string) -> string:
     return encrypt(ciphertext, key, True)
 
 
-dd = encrypt("яяяЯЯЯ", "абв")
-print(dd)
-print(decrypt(dd, "абв"))
+text = "Реализовать шифрование и дешифрацию файла по методу Виженера с составным ключом."
+keys = "итмо крутой университет".split()
+print("текст: " + text)
+
+for key in keys:
+    text = encrypt(text, key)
+print("зашифрованный текст: " + text)
+
+for key in keys:
+    text = decrypt(text, key)
+print("расшифрованный текст: " + text)
+
